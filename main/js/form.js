17b726024569b4d23055808b2c7a4a45af89cd63
Samar Pratap <92373593+noughtsamar@users.noreply.github.com>
2022-09-10 21:32:49 +0530

    Add files via upload

---
+++ main/js/form.js
@@ -0,0 +1,13 @@
+function myfun()
+{
+ 
+    var name = document.getElementsByClassName('nametext')[0].value;
+    var age = document.getElementsByClassName('agetext')[0].value;
+    console.log(age)
+    // var gender  = document.getElementById('gender').value;
+    
+    // var data = {name,gender}
+    // console.log("a")
+    // console.log(data)
+    
+}
